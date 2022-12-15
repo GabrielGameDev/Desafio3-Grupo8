8da65e5aef8455d1d587a3b82ef213f37d818f53
GabrielGameDev <gabrielgamedev@gmail.com>
2022-12-15 19:30:36 -0300

    Login retorna token
    Login de um psicologo retorna um token

---
+++ src/configs/secret.js
@@ -0,0 +1,3 @@
+module.exports = {
+    key: "desafio3gama",
+}
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,24 +1,29 @@
-const {Psicologos} = require('../models');
+const { Psicologos } = require('../models');
 const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const secret = require('../configs/secret');
 
 const AuthController = {
     async login(req, res) {
-        const {email, senha} = req.body;
+        const { email, senha } = req.body;
 
-        const psicologo = await Psicologos.findOne({where: {email}});
-        if(!psicologo) {
+        const psicologo = await Psicologos.findOne({ where: { email } });
+        if (!psicologo) {
             return res.status(404).json("Email não encontrado");
         }
 
-        if(!bcrypt.compareSync(senha, psicologo.senha)) {
+        if (!bcrypt.compareSync(senha, psicologo.senha)) {
             return res.status(401).json("Senha incorreta");
         }
 
-        // const token = jwt.sign({id:
-        // psicologo.id}, process.env.SECRET, {expiresIn: '1d'});
-        // res.json({token});
-        res.json("usuário logado")
+        const token = jwt.sign(
+            {
+                id: psicologo.id,
+                email: psicologo.email,
+                nome: psicologo.nome,
+            },
+            secret.key);
+        res.json(token);
     }
 }
 
